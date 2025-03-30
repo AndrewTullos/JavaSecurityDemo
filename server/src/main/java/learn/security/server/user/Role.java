86472c110f16ce3fb82233d137f3a90caac1cf5c
Drew <tullos.andrew@gmail.com>
2025-03-29 19:08:21 -0500

    UserRepo Interface

---
+++ server/src/main/java/learn/security/server/user/Role.java
@@ -0,0 +1,5 @@
+package learn.security.server.user;
+
+public enum Role {
+    USER, ADMIN
+}
--- server/src/main/java/learn/security/server/user/User.java
+++ server/src/main/java/learn/security/server/user/User.java
@@ -23,7 +23,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @AllArgsConstructor
 @Entity
 @Table(name = "_user")
-public class User {
+public class User implements UserDetails {
     // Acts as model (Getter/Setters) for User entity
 
     @Id
@@ -34,4 +34,41 @@ public class User {
     private String email;
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
