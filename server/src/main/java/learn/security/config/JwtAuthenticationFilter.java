dfbfada7acd80dbfda0187ad8d4e50440e4c5f2c
Drew <tullos.andrew@gmail.com>
2025-03-31 12:25:02 -0500

    Service Layer JWT

--- server/src/main/java/learn/security/config/JwtAuthenticationFilter.java
+++ server/src/main/java/learn/security/config/JwtAuthenticationFilter.java
@@ -5,11 +5,18 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(
@@ -17,6 +24,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    // todo: extract email from jwt token
     }
 }
